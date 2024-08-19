2eaa14ffe4dd12bb44d5db46d7ec37002570cd88
AragaoMV <marcosvinisa@icloud.com>
2024-08-19 17:50:31 -0300

    Bonus challenge Hard level, camera switcher

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -3,19 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
-{   
+{
     public GameObject player;
-    private Vector3 offset = new Vector3(0,5,-7);
+    private Vector3 mainCamera = new Vector3(0, 5, -7);
+    private Vector3 frontCamera = new Vector3(-0.3f, 2, 1);
+    private Vector3 currentCameraOffset;
+    private int cameraIndex = 0; //Index camera (0->Main, 1-> Front Camera)
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentCameraOffset = mainCamera;
     }
 
     // Update is called once per frame at the end
     void LateUpdate()
     {
-        //Offset the camera behind the player by adding to the player's position
-        transform.position = player.transform.position + offset;
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            cameraIndex = (cameraIndex + 1) % 2  //amount of cameras available;
+            switch (cameraIndex)
+            {
+                case 0:
+                    currentCameraOffset = mainCamera;           //Camera behind the player
+                    break;
+                case 1:
+                    currentCameraOffset = frontCamera;         //Camera at the hood of the car
+                    break;
+            }
+        }
+        
+        transform.position = player.transform.position + currentCameraOffset;
     }
 }
+
