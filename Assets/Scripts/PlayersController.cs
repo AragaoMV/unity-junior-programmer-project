68881f7743ffda6b69b11b22ee3866e007f79944
AragaoMV <marcosvinisa@icloud.com>
2024-08-19 19:24:56 -0300

    Local Multiplayer

---
+++ Assets/Scripts/PlayersController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayersControllers : MonoBehaviour
+{
+    //Player's pribvate Variables
+    private float speed = 15.0f;
+    private float turnSpeed = 70.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    public string inputID;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Inputing player's controlls
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        verticalInput = Input.GetAxis("Vertical" + inputID);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);  //Vehicle moving forward 
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);    //Vehicle turns
+    }
+}
